526cb92f911f04180916d12a9be94856b0f3f01a
Anum Qudsia <Anum.Qudsia@dai.co.uk>
2011-07-05 13:42:10 +0100

    Finished first cut of the User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110701120325) do
+ActiveRecord::Schema.define(:version => 20110705114007) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -18,5 +18,7 @@ ActiveRecord::Schema.define(:version => 20110701120325) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
